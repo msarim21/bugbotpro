a2aaf7a1db7fb08d0286312394e84d58104cff64
SARIM <msarim7898@gmail.com>
2026-06-30 16:59:30 +0500

    feat: add vite config

---
+++ client/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+
+  export default defineConfig({
+    plugins: [react()],
+    build: { outDir: 'dist' },
+    server: { port: 5000, host: '0.0.0.0', proxy: { '/api': 'http://localhost:3000' } }
+  });
+  
